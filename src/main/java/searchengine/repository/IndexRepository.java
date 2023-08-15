fdda8ba214a47b3fbfc537dcac8fa42080ce8e3f
Vsevolod Konkin <vo.konkin@gmail.com>
2023-08-15 14:24:09 +0300

    27-changed-indexRepository-added-getSnippet-and-added-getRelevance

--- src/main/java/searchengine/repository/IndexRepository.java
+++ src/main/java/searchengine/repository/IndexRepository.java
@@ -2,11 +2,18 @@ package searchengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import searchengine.model.Index;
+
+import java.util.List;
 
 @Repository
 public interface IndexRepository extends JpaRepository<Index, Long> {
     @Query(value = "SELECT * FROM search_engine.search_index WHERE (:lemmaId IS NULL OR lemma_id LIKE :lemmaId)", nativeQuery = true)
     Index findByLemmaId(Long lemmaId);
+    @Query(value = "SELECT * FROM search_engine.search_index si WHERE si.lemma_id = " +
+            "(SELECT l.id FROM lemma l WHERE l.lemma = :lemma) AND " +
+            "si.page_id IN (SELECT p.id FROM page p WHERE p.site_id = :siteId)", nativeQuery = true)
+    List<Index> findByLemmaAndSite(@Param("lemma") String lemma, @Param("siteId") long siteId);
 }
--- src/main/java/searchengine/searching/Searching.java
+++ src/main/java/searchengine/searching/Searching.java
@@ -1,9 +1,14 @@
 package searchengine.searching;
 
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import searchengine.dto.search.DetailedSearchData;
 import searchengine.dto.search.SearchData;
 import searchengine.lemma.LemmaFinder;
+import searchengine.model.Index;
+import searchengine.model.Lemma;
 import searchengine.model.SiteModel;
 import searchengine.repository.IndexRepository;
 import searchengine.repository.LemmaRepository;
@@ -11,10 +16,7 @@ import searchengine.repository.PageRepository;
 import searchengine.repository.SiteRepository;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RequiredArgsConstructor
 public class Searching {
@@ -31,8 +33,8 @@ public class Searching {
         if (url == null) {
             listSites = siteRepository.findAll();
         } else {
-           SiteModel site = siteRepository.findByUrl(url);
-           listSites = new ArrayList<>(Collections.singletonList(site));
+            SiteModel site = siteRepository.findByUrl(url);
+            listSites = new ArrayList<>(Collections.singletonList(site));
         }
         for (SiteModel site : listSites) {
             DetailedSearchData data = getDetailedSearchData(query, site);
@@ -48,7 +50,7 @@ public class Searching {
         DetailedSearchData detSearchData = new DetailedSearchData();
         detSearchData.setSite(site.getUrl());
         detSearchData.setSiteName(site.getName());
-        detSearchData.setSnippet(getSnippet(query));
+        detSearchData.setSnippet(getSnippet(query, site.getUrl()));
         detSearchData.setRelevance(getRelevance(query, site));
         return detSearchData;
     }
@@ -56,15 +58,79 @@ public class Searching {
     private double getRelevance(String query, SiteModel site) {
         try {
             LemmaFinder lemmaFinder = LemmaFinder.getInstance();
-            Map<String, Integer> lemmaMap = lemmaFinder.collectLemmas(query);
+            Set<String> filteredLemmas = lemmaFinder.getLemmaSet(query);
+
+            List<String> sortedLemmas = new ArrayList<>(filteredLemmas);
+            sortedLemmas.sort(Comparator.comparingInt(lemma -> {
+                Lemma lemmaModel = lemmaRepository.findLemma(lemma);
+                return lemmaModel != null ? lemmaModel.getFrequency() : Integer.MAX_VALUE;
+            }));
+
+            double maxRelevance = 0.0;
+            double totalRelevance = 0.0;
+
+            List<Index> currentPages = new ArrayList<>();
+
+            int maxPageCount = 10;
+
+            for (String lemma : sortedLemmas) {
+                List<Index> indexEntries = indexRepository.findByLemmaAndSite(lemma, site.getId());
+                if (indexEntries.size() > maxPageCount) {
+                    continue;
+                }
+
+                if (currentPages.isEmpty()) {
+                    currentPages.addAll(indexEntries);
+                } else {
+                    currentPages.removeIf(index -> !indexEntries.contains(index));
+                }
+
+                if (currentPages.isEmpty()) {
+                    break;
+                }
+                double rank = currentPages.stream().mapToDouble(Index::getRank).sum();
+                totalRelevance += rank;
+
+                maxRelevance += lemmaRepository.findLemma(lemma).getFrequency();
+            }
+
+            if (maxRelevance == 0.0) {
+                return 0.0;
+            }
+
+            return totalRelevance / maxRelevance;
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return 0.0;
     }
 
-    private String getSnippet(String query) {
-        return null;
+    private String getSnippet(String query, String url) {
+        try {
+            String pageContent = getPageContent(url);
+            String highlightedContent = highlightQueryMatches(pageContent, query);
+            String snippet = limitSnippetLength(highlightedContent);
+            return snippet;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to fetch webpage content " + e);
+        }
+    }
+
+    private String getPageContent(String url) throws IOException {
+        Connection connection = Jsoup.connect(url);
+        Document document = connection.get();
+        String content = document.text();
+        return content;
+    }
+
+    private String highlightQueryMatches(String content, String query) {
+        String highlightedContent = content.replaceAll(query, "<b>$0</b>");
+        return highlightedContent;
+    }
+
+    private String limitSnippetLength(String content) {
+        int maxSnippetLength = 300;
+        String snippet = content.substring(0, Math.min(content.length(), maxSnippetLength));
+        return snippet;
     }
 }
